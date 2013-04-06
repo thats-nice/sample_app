a0361aed9002a64ee2a545d038cf1da102df3886
Jeff <chienj.22@gmail.com>
2013-04-07 00:15:30 +0200

    Working one starting from 4

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.13'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
+  gem 'guard-rspec', '1.2.1'
 end
 
 # Gems used only for assets and not required
@@ -19,6 +20,10 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0'
+  gem 'wdm', '~>0.1'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,31 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
+	
+	it "should have the right title" do
+	  visit '/static_pages/home'
+	  page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App")
+	end
+	
+	it "should not have a custom page title" do
+		visit '/static_pages/home'
+		page.should_not have_selector('title', :text=> '| Home')
+	end
   end
   
   describe "Help page" do
 	
 		it "should have the content 'Help'" do
 		visit '/static_pages/help'
-		page.should have_content('Help')
+		page.should have_selector('h1', :text => 'Help')
+		end
+		
+		it "should have the right title 'Help page'" do
+		visit '/static_pages/help'
+		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
 		end
 	end 
 	
@@ -20,7 +36,12 @@ describe "StaticPages" do
 	
 		it "should have the content 'About Us'" do
 		visit '/static_pages/about'
-		page.should have_content('About Us')
+		page.should have_selector('h1', :text => 'About Us')
+		end
+		
+		it "should have the right title 'Help page'" do
+		visit '/static_pages/about'
+		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
 		end
 	end
 end
